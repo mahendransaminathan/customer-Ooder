73d26eebc99ce6a89884e2c14c878ded7434961a
Mahe Sami <mahendransamynathan@gmail.com>
2025-06-22 22:06:00 +0100

    Updated

--- Program.cs
+++ Program.cs
@@ -5,26 +5,24 @@ using CustomerOrder.Services;
 using Blazored.LocalStorage;
 using CustomerOrder; // Add this line if your App.razor is in the root namespace 'CustomerOrder'
 
-
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddBlazoredLocalStorage();
 // Ensure JwtAuthStateProvider is defined in your project, e.g., in CustomerOrder/Authentication/JwtAuthStateProvider.cs
 builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthStateProvider>();
 // Register the handler
 builder.Services.AddScoped<AuthHeaderHandler>();
-builder.Services.AddScoped<AuthHeaderHandler>();
 
 // Inject HttpClient that uses the handler
-builder.Services.AddScoped(sp =>
-    new HttpClient(sp.GetRequiredService<AuthHeaderHandler>())
-    {
-        BaseAddress = new Uri("http://localhost:5277/")
-    });
+builder.Services.AddScoped<AuthHeaderHandler>();
+
+builder.Services.AddHttpClient("AuthenticatedClient", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5277/");
+})
+.AddHttpMessageHandler<AuthHeaderHandler>();
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("AuthenticatedClient"));
 
 await builder.Build().RunAsync();
